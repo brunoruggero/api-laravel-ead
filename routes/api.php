6aa58dc8d16f192f6bbbdb3cda5ca7cd7f881b5a
Bruno Ruggero <bruno.ruggero@gmail.com>
2023-06-28 12:07:39 -0300

    ADD: Authenticator with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -7,20 +7,25 @@ use App\Http\Controllers\Api\{
     ReplySupportController,
     SupportController
 };
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/courses', [CourseController::class, 'index']);
-Route::get('/courses/{id}', [CourseController::class, 'show']);
-Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+Route::post('/auth', [AuthController::class, 'auth']);
 
-Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
-Route::get('/lessons/{id}', [LessonController::class, 'show']);
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
+    Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
 
-Route::get('/my-supports', [SupportController::class, 'mySupports']);
-Route::get('/supports', [SupportController::class, 'index']);
-Route::post('/supports', [SupportController::class, 'store']);
+    Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+    Route::get('/lessons/{id}', [LessonController::class, 'show']);
 
-Route::post('/replies', [ReplySupportController::class, 'createReply']);
+    Route::get('/my-supports', [SupportController::class, 'mySupports']);
+    Route::get('/supports', [SupportController::class, 'index']);
+    Route::post('/supports', [SupportController::class, 'store']);
+
+    Route::post('/replies', [ReplySupportController::class, 'createReply']);
+});
 
 Route::get('/', function (){
     return response()->json([
